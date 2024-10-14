cd0f519754083e04061c06b41361f33f582a7e1e
skaym00t <cerber.lv.23@gmail.com>
2024-10-14 09:31:30 +0400

    update files, return name user

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,4 +1,7 @@
+
+
 def welcome_user():
     import prompt
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
+    return name
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -4,8 +4,8 @@
 def main():
     print('Welcome to the Brain Games!')
     from brain_games.cli import welcome_user
-    welcome_user()
-
+    name = welcome_user()
+    return name
 
 if __name__ == '__main__':
     main()
