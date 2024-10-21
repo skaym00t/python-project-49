db0544a8b123b22c595f7c4986cc18c9ec5e8837
skaym00t <cerber.lv.23@gmail.com>
2024-10-21 10:45:08 +0400

    fix lint flake8

--- brain_games/games/game_5.py
+++ brain_games/games/game_5.py
@@ -1,24 +1,30 @@
+def calculate_question():
+    import random
+    number_1 = random.randint(1, 100)
+    if number_1 == 1:
+        correct_answer = 'no'
+    elif number_1 <= 3:
+        correct_answer == 'yes'
+    else:
+        for i in range(2, number_1):
+            if number_1 % i == 0:
+                correct_answer = 'no'
+                break
+            else:
+                correct_answer = 'yes'
+                continue
+    return [number_1, correct_answer]
+
+
 def result_game():
+    from brain_games.games.game_5 import calculate_question
     print('Answer "yes" if given number is prime. Otherwise answer "no".')
     import prompt
-    import random
     lose = 0
     correct_answer = ''
     user_answer = ''
     for i in range(3):
-        number_1 = random.randint(1, 100)
-        if number_1 == 1:
-            correct_answer = 'no'
-        elif number_1 == 2 or number_1 == 3:
-            correct_answer == 'yes'
-        elif number_1 > 3:
-            for i in range(2, number_1):
-                if number_1 % i == 0:
-                    correct_answer = 'no'
-                    break
-                elif number_1 % i != 0:
-                    correct_answer = 'yes'
-                    continue
+        number_1, correct_answer = calculate_question()
         print(f'Question: {number_1}')
         user_answer = prompt.string('Your answer: ')
         if user_answer == correct_answer:
